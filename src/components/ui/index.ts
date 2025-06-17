fb5c6f6a68f3c738de47c6e76106551e96d517d2
EBOLABOY <1242772513@qq.com>
2025-06-17 18:07:38 +0800

    修复构建依赖问题
    1. 将tailwindcss和postcss、autoprefixer移到dependencies中，因为构建时需要这些依赖
    2. 创建UI组件的index.ts文件，确保正确导出GlassCard等组件
    3. 这应该解决Vercel构建时找不到tailwindcss和GlassCard组件的问题

---
+++ src/components/ui/index.ts
@@ -0,0 +1,12 @@
+export { 
+  GlassCard, 
+  GlassButton, 
+  GlassInput, 
+  GlassNav, 
+  LoadingGlass, 
+  GlassContainer 
+} from './GlassCard'
+
+export { AnalysisTimeline } from './AnalysisTimeline'
+export { MindMapVisualization } from './MindMapVisualization'
+export { ThinkingAnimation } from './ThinkingAnimation'
